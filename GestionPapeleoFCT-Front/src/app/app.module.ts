f6651e99c3c9703e09526e60d4ff920a0667fee9
Mery <maria.juan.vi@gmail.com>
2021-01-25 23:47:21 +0100

    Base crud curso-alumnos + base login

--- GestionPapeleoFCT-Front/src/app/app.module.ts
+++ GestionPapeleoFCT-Front/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+//COMPONENTES
+import { MenuComponent } from './menu/menu.component';
+import { NuevoAlumnoComponent } from './componentes/nuevo-alumno/nuevo-alumno.component';
+import { ListaCursosComponent } from './componentes/lista-cursos/lista-cursos.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ListaAlumnosComponent } from './componentes/lista-alumnos/lista-alumnos.component';
+import { AlumnoComponent } from './componentes/alumno/alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoAlumnoComponent,
+    MenuComponent,
+    ListaCursosComponent,
+    LoginComponent,
+    ListaAlumnosComponent,
+    AlumnoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ GestionPapeleoFCT-Front/src/app/componentes/nuevo-alumno/nuevo-alumno.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NuevoAlumnoComponent } from './nuevo-alumno.component';
+
+describe('NuevoAlumnoComponent', () => {
+  let component: NuevoAlumnoComponent;
+  let fixture: ComponentFixture<NuevoAlumnoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NuevoAlumnoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NuevoAlumnoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
