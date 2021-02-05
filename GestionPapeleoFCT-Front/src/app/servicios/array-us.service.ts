99d3e7172de15ca521acc3db1c3a83f44e634021
Mery <maria.juan.vi@gmail.com>
2021-02-05 13:28:17 +0100

    Registro inicial

--- GestionPapeleoFCT-Front/src/app/servicios/array-us.service.ts
+++ GestionPapeleoFCT-Front/src/app/servicios/array-us.service.ts
@@ -7,17 +7,23 @@ export class ArrayUsService {
   public static readonly SESSION_STORAGE_KEY: string = "apiPassport";
   email:string;
   dni:string;
+  rol:any;
 
   constructor() {
     this.email = '';
     this.dni = '';
+    this.rol = 0;
   }
   
-  setArray(email: string,dni: string) {
+  setArray(email: string,dni: string,rol: any) {
     this.email = email;
     this.dni = dni;
+    this.rol =rol;
   }
   
+  getRol() {
+    return this.rol;
+  }
   getEmail() {
     return this.email;
   }
--- GestionPapeleoFCT-Front/src/app/servicios/registro-persona.service.ts
+++ GestionPapeleoFCT-Front/src/app/servicios/registro-persona.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import * as _ from "lodash";
+import { ArrayUsService } from 'src/app/servicios/array-us.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,13 @@ import * as _ from "lodash";
 export class RegistroPersonaService {
 
   public static readonly SESSION_STORAGE_KEY: string = "apiPassport";
-  message: string;
+  message: any;
   user: any;
 
-  constructor(private router: Router, private http: HttpClient) {
-    this.user = {
-      access_token: "",
-      email: ""
-    }
+  constructor(private router: Router, private http: HttpClient, private ArrayUsService: ArrayUsService) {
     this.message = "";
   }
-  public Registro = (email:string, dni: string, nombre: string, apellidos: string, localidad: string,residencia: string, tlf: number) => {
+  public Registro = (email:any, dni: any, nombre: any, apellidos: any, localidad: any,residencia: any, tlf: any) => {
     const url = "http://localhost:8000/api/register_persona";
 
     let headers = new HttpHeaders({
@@ -27,20 +24,4 @@ export class RegistroPersonaService {
     });
     return this.http.post(url, {'email' : email, 'dni': dni, 'nombre' : nombre, 'apellidos': apellidos, 'localidad': localidad, 'residencia': residencia , 'tlf': tlf }, { headers: headers });
   };
-  
-  public registroSuscription(email: string, dni: string, nombre: string, apellidos: string, localidad: string,residencia: string, tlf: number) {
-    this.Registro(email, dni, nombre, apellidos, localidad, residencia, tlf).subscribe(
-      (response: any) => { 
-        console.log(response);
-        this.message = "Registro correcto";
-        this.user.access_token = response['message']['access_token'];
-        this.user.email = response.message.user.email;
-        sessionStorage.setItem(RegistroPersonaService.SESSION_STORAGE_KEY, JSON.stringify(this.user));
-        this.router.navigate(['listaCursos']);
-      },
-      (error) => {
-        this.message = error.error.message;
-      }
-    );
-  }
 }
