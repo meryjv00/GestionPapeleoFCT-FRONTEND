9880a852525ebedc48d865c2b790753a4e170223
Mery <maria.juan.vi@gmail.com>
2021-02-19 17:15:20 +0100

    env

--- GestionPapeleoFCT-Front/src/environments/environment.prod.ts
+++ GestionPapeleoFCT-Front/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  dirBack: 'http://coworkingdaw2.ddns.net:8081/api/',
+  dirBack: 'http://localhost:8000/api/',
+  //dirBack: 'http://coworkingdaw2.ddns.net:8081/api/',
   production: true
 };
