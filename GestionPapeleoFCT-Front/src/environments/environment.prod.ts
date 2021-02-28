0bf9c6162252b1a554ca33de25bead53bc0bf427
Mery <maria.juan.vi@gmail.com>
2021-02-28 18:47:05 +0100

    Redirect netlify

--- GestionPapeleoFCT-Front/src/environments/environment.prod.ts
+++ GestionPapeleoFCT-Front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //dirBack: 'http://localhost:8000/api',
+  //dirBack: 'http://localhost:8000/api/',
   //dirBack2: 'http://localhost:8000/',
   dirBack: 'https://95.39.184.89/api/',
   dirBack2: 'https://95.39.184.89/',
