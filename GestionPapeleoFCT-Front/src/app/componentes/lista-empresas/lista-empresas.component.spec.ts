b2133a27b71bb73d799237fb425d0fdf774ef8a7
Daniel Sánchez <danielsanchezc6@gmail.com>
2021-03-08 22:05:28 +0100

    Error solucionado

--- GestionPapeleoFCT-Front/src/app/componentes/lista-empresas/lista-empresas.component.spec.ts
+++ GestionPapeleoFCT-Front/src/app/componentes/lista-empresas/lista-empresas.component.spec.ts
@@ -5,8 +5,6 @@ import { ListaEmpresasComponent } from './lista-empresas.component';
 import { AdminEmpresasService } from "src/app/servicios/admin-empresas.service";
 import { CompartirDatosService } from 'src/app/servicios/compartir-datos.service';
 import { AnexosService } from 'src/app/servicios/anexos.service';
-import { HttpClientModule } from '@angular/common/http';
-import {RouterTestingModule} from "@angular/router/testing";
 
 
 describe('ListaEmpresasComponent', () => {
